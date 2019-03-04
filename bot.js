b6c4a094f7ff41a480f7566633082dfb0c54a161
Lancelot2023 <48219101+Lancelot2023@users.noreply.github.com>
2019-03-04 23:43:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,13 +13,37 @@ client.on('message', message => {
   	}
 });
 
-Client.on('message' , (message)=>{
-    if(!message.content.startsWith(prefix)) return;
+ if(message.content.startsWith(prefix + "prune")){
+        let args = message.content.split(" ").slice(1);
+        let author = message.member;
+        let role = message.guild.roles.find('name' , "atSpace Gaming");
+        if(author.roles.has(role.id)){
+            if(!args[0]){
+                message.delete();
+                message.author.send("No arguments given.");
+                return;
+            }
+            if(args[0] > 100){
+                message.delete();
+                message.author.send("Maximum is 100 messages at once.");
+                return;    
+            }
 
-    if(message.content.startsWith(prefix + "hello")){
-        message.channel.send("Hello.");
+            message.delete();
+            message.channel.bulkDelete(args[0]);
+            message.author.send({embed:{
+                color: 0x28d62b,
+                description: "Done, I have deleted " + args[0] + " messages."
 
-    }
+            }})
+            return;    
+        }
+
+        
+    }   
+    
+    
+})
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
