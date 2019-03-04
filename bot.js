66c6bf0702dcd277d3af332b5559aff216adaec3
Lancelot2023 <48219101+Lancelot2023@users.noreply.github.com>
2019-03-05 00:06:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "!";
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -13,37 +12,6 @@ client.on('message', message => {
   	}
 });
 
- if(message.content.startsWith(prefix + "prune")){
-        let args = message.content.split(" ").slice(1);
-        let author = message.member;
-        let role = message.guild.roles.find('name' , "atSpace Gaming");
-        if(author.roles.has(role.id)){
-            if(!args[0]){
-                message.delete();
-                message.author.send("No arguments given.");
-                return;
-            }
-            if(args[0] > 100){
-                message.delete();
-                message.author.send("Maximum is 100 messages at once.");
-                return;    
-            }
-
-            message.delete();
-            message.channel.bulkDelete(args[0]);
-            message.author.send({embed:{
-                color: 0x28d62b,
-                description: "Done, I have deleted " + args[0] + " messages."
-
-            }})
-            return;    
-        }
-
-        
-    }   
-    
-    
-})
-
+ 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
